088a7933fea8fea313d7e7d3be305c2c46c47c49
Joshua Adams <joshuaadams031999@gmail.com>
2022-08-12 09:51:44 +1000

    Week 4 Tutorial setup

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  accountsArray = [
+    {userName: "abc@gmail.com", password: "123"},
+    {userName: "bbb@gmail.com", password: "222"},
+    {userName: "ccc@gmail.com", password: "333"},
+  ]
+
+  userName = "";
+  password = "";
+
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  formSubmit() {    
+    var valid: boolean = false
+    this.accountsArray.map((el) => {
+      if(el.userName == this.userName && el.password == this.password){
+        valid = true
+      }
+    })
+
+    if(valid){
+      this.redirectAccountPage();
+    };
+
+  };
+
+  redirectAccountPage() {
+    console.log("redirected!")
+    this.router.navigateByUrl('/account')
+  };
+
+}
